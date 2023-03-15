390f3c203a4ef0b2db655ea87c510a510dde934b
Maksim Saykovsky <saykovskiy.maks03@gmail.com>
2023-03-15 20:50:31 +0300

    CustomTokenObtainPairView created
    Now we can get user's username from token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
+from accounts.views import CustomTokenObtainPairView
+
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
