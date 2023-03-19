9d07bed19674445febb52f7350283973b1cf843f
Maksim Saykovsky <saykovskiy.maks03@gmail.com>
2023-03-19 14:36:11 +0300

    added MessageView and url for it

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Message
+from .serializers import MessageSerializer
 
 
 def index(request):
@@ -7,3 +11,9 @@ def index(request):
 
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
