989a9e9fad0e7340fa2f92ce018b967689307569
Ash1VT <nikita.panasevitch@yandex.by>
2023-03-19 16:24:01 +0300

    fixed server error with InvalidToken

--- accounts/services.py
+++ accounts/services.py
@@ -1,4 +1,5 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
 from channels.db import database_sync_to_async
 from django.contrib.auth.models import User, AnonymousUser
 
@@ -6,7 +7,11 @@ from django.contrib.auth.models import User, AnonymousUser
 @database_sync_to_async
 def get_user(auth_header):
     auth = JWTAuthentication()
-    validated_token = auth.get_validated_token(auth_header)
+    try:
+        validated_token = auth.get_validated_token(auth_header)
+    except InvalidToken:
+        return AnonymousUser()
+
     user_id = validated_token['user_id']
     try:
         return User.objects.get(id=user_id)
