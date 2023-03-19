fe5ee9b11ae5426cbf0d0bfa83d96d47976de7b7
Ash1VT <nikita.panasevitch@yandex.by>
2023-03-19 16:29:01 +0300

    edited authentication procedure in consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,6 +3,7 @@ from json.decoder import JSONDecodeError
 
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
 from chat.services import MessageService
 from django.contrib.auth.models import User
 
