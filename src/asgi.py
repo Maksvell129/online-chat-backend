db003ee2a8aa6801a75ef12c6bde4d06eacc2e55
Ash1VT <nikita.panasevitch@yandex.by>
2023-03-19 16:26:37 +0300

    edited for postman support

--- src/asgi.py
+++ src/asgi.py
@@ -4,6 +4,7 @@ import os
 from accounts.middlewares import JWTAuthMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import OriginValidator
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
@@ -16,10 +17,11 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
+        "websocket": OriginValidator(
             JWTAuthMiddleware(
                 URLRouter(websocket_urlpatterns)
-            )
-        ),
+            ),
+            ['*']
+        )
     }
 )
