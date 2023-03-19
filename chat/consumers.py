61ddaf9beecf10669273f1949d82288975eeb575
Ash1VT <nikita.panasevitch@yandex.by>
2023-03-19 16:29:01 +0300

    edited authentication procedure in consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,30 +1,34 @@
-import json
+from json import loads, dumps
+from json.decoder import JSONDecodeError
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope['user']
+        self.user: User = self.scope['user']
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
-        if self.user.is_authenticated:
+        await self.accept()
+        if not self.user.is_anonymous:
             # Join room group
             await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-            await self.accept()
         else:
-            await self.close()
+            await self.close(code=4003)
 
     async def disconnect(self, close_code):
         # Leave room group
-        print('test')
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        try:
+            text_data_json = loads(text_data)
+            message = text_data_json["message"]
+        except JSONDecodeError:
+            message = text_data
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -36,4 +40,4 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = event["message"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=dumps({"message": message, 'sender': self.user.username}))
