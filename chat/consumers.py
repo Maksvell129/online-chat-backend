4c8c12a75feabe99889687f7649dbc90b34028a7
Maksim Saykovsky <saykovskiy.maks03@gmail.com>
2023-04-02 20:00:09 +0300

    added message types, message history and notifications when user enters and leaves chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,6 +3,8 @@ from json.decoder import JSONDecodeError
 
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from chat.serializers import MessageSerializer
 from chat.services import MessageService
 from django.contrib.auth.models import User
 
@@ -13,15 +15,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = "chat"
 
         await self.accept()
-        if not self.user.is_anonymous:
-            # Join room group
-            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        else:
+        if self.user.is_anonymous:
             await self.close(code=4003)
+
+        # Join room group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "user_join",
+                "username": self.user.username,
+            },
+        )
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "message_history",
+            },
+        )
 
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+        # send the leave event to the room
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "user_leave",
+                "username": self.user.username,
+            },
+        )
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     # Receive message from WebSocket
     async def receive(self, text_data):
@@ -31,16 +61,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
         except JSONDecodeError:
             message = text_data
 
-        await database_sync_to_async(MessageService.save_message)(message, self.user)
+        message = await database_sync_to_async(MessageService.save_message)(message, self.user)
+        serialized_message = MessageSerializer(message).data
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name, {"type": "chat_message", "message": serialized_message}
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
-
         # Send message to WebSocket
-        await self.send(text_data=dumps({"message": message, 'sender': self.user.username}))
+        await self.send(text_data=dumps({
+            "type": "chat_message",
+            "message": message,
+        }))
+
+    async def user_join(self, event):
+        username = event["username"]
+        await self.send(text_data=dumps({
+            "type": "user_join",
+            "username": username
+        }))
+
+    async def user_leave(self, event):
+        username = event["username"]
+        await self.send(text_data=dumps({
+            "type": "user_leave",
+            "username": username
+        }))
+
+    async def message_history(self, event):
+        messages = await database_sync_to_async(MessageService.get_all_messages_serialized)()
+
+        await self.send(text_data=dumps(
+            {
+                "type": "message_history",
+                "messages": messages,
+            })
+        )
