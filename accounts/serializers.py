08e2b95464d16541796a4661da43b96ddea1e6e7
Maksim Saykovsky <saykovskiy.maks03@gmail.com>
2023-04-11 20:41:19 +0300

    added password and email validation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,10 @@
+import re
+
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from src.constant import MIN_PASSWORD_LENGTH
 
 User = get_user_model()
 
@@ -23,6 +27,18 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_email(self, value):
+        if not re.match(r"/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/", value):
+            raise serializers.ValidationError('Please enter a valid email address.')
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('This email is already in use.')
+        return value
+
+    def validate_password(self, value):
+        if len(value) < MIN_PASSWORD_LENGTH:
+            raise serializers.ValidationError(f'Password must be at least {MIN_PASSWORD_LENGTH} characters.')
+        return value
 
     def create(self, validated_data):
         user = User.objects.create(
